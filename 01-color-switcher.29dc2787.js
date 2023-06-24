dbbdf527e0cc5f678a72842d85f478852ddfac5e
Rhilim <110243763+Rhilim@users.noreply.github.com>
2023-06-24 21:44:06 +0000

    Deploying to gh-pages from @ Rhilim/goit-js-hw-09@bd21d8e28fe18f6c3008b2d1c24a648a80357564 🚀

--- 01-color-switcher.29dc2787.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.29dc2787.js.map
